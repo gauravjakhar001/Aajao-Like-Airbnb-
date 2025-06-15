f1418a97270dc1c9abbe28fc5f2dc3f692b4bb1d
Gaurav Jakhar <jakhargaurav001@gmail.com>
2025-06-15 19:45:37 +0530

    Logo Added

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/aajao.png";
 // import Login from "../Components/Login";
 import SignUp from "../Components/SignUp";
 import Loginkaro from "../Components/Loginkaro";
